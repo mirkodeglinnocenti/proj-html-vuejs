ca7ea168be85a2c69cac5bfe87866ac1ff944446
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-03-13 15:33:07 +0100

    FEAT Header layout + menu with data added

---
+++ src/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+    <header class="main-header">
+        <div class="container-large">
+            <div class="row">
+                <div class="col">
+                    <a href="#">
+                        <img src="/img/dark-logo.png" alt="logo MaxCoach">
+                    </a>
+                </div>
+                <div class="col">
+                    <ul class="menu-list row">
+                        <li v-for="el in menu"><a href="#">{{ el }}</a></li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <ul class="row">
+                        <li><a href="#"><font-awesome-icon icon="fa-brands fa-twitter" /></a></li>
+                        <li><a href="#"><font-awesome-icon icon="fa-brands fa-facebook-f" /></a></li>
+                        <li><a href="#"><font-awesome-icon icon="fa-brands fa-instagram" /></a></li>
+                        <li><a href="#"><font-awesome-icon icon="fa-brands fa-linkedin" /></a></li>
+                    </ul>
+                    
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        data (){
+            return{
+                menu: [
+                    "Home", "Pages", "Courses", "Features", "Blog", "Shop"
+                ]
+            }
+        }
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+.main-header{
+    background-color: aqua;
+    padding: 40px;
+
+    img{
+        width: 160px;
+    }
+}
+.row{
+    justify-content: space-between;
+    align-items: center;
+}
+
+.menu-list{
+    gap: 10px;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,21 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faFacebookF, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+
+/* add icons to the library */
+library.add(faUserSecret, faTwitter, faFacebookF, faInstagram, faLinkedin)
+
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
